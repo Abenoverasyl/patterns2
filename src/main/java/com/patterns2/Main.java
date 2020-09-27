d40a1667398d8f0387a360cc182d7da9e7adb86b
YAben <yaben@beeline.kz>
2020-09-27 23:31:58 +0600

    adapter pattern last

--- src/main/java/com/patterns2/Main.java
+++ src/main/java/com/patterns2/Main.java
@@ -1,16 +1,14 @@
 package com.patterns2;
 
-import com.patterns2.adapter.CaramelFilter;
+import com.patterns2.adapter.CaramelAdapter;
 import com.patterns2.adapter.Image;
 import com.patterns2.adapter.ImageView;
-import com.patterns2.adapter.VividFilter;
-import com.patterns2.adapter.avaFilters.Caramel;
 import lombok.var;
 
 public class Main {
     public static void main(String[] args) {
         var imageView = new ImageView(new Image());
 
-        imageView.apply(new CaramelFilter(new Caramel() ));
+        imageView.apply(new CaramelAdapter());
     }
 }
