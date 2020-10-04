5233c97d05f2c2d8b4d0ac50327c017c0495020a
YAben <yaben@beeline.kz>
2020-10-04 22:22:15 +0600

    facade pattern

--- src/main/java/com/patterns2/Main.java
+++ src/main/java/com/patterns2/Main.java
@@ -1,18 +1,11 @@
 package com.patterns2;
 
-import com.patterns2.decorator.CloudStream;
-import com.patterns2.decorator.Stream;
-import com.patterns2.facade.Message;
-import com.patterns2.facade.NotificationServer;
+import com.patterns2.facade.NotificationService;
 import lombok.var;
 
 public class Main {
     public static void main(String[] args) {
-        var server = new NotificationServer();
-        var connection = server.connect("ip");
-        var authToken = server.authenticate("appID", "key");
-        var message = new Message("Hello World");
-        server.send(authToken, message, "target");
-        connection.disconnect();
+        var service = new NotificationService();
+        service.send("Hello world", "target");
     }
 }
