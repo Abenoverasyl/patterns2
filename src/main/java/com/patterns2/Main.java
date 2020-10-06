966663f97fe5d7f07074d7effe7a782f6a3c28be
YAben <yaben@beeline.kz>
2020-10-07 00:58:48 +0600

    problem for proxy pattern

--- src/main/java/com/patterns2/Main.java
+++ src/main/java/com/patterns2/Main.java
@@ -1,12 +1,17 @@
 package com.patterns2;
 
-import com.patterns2.bridge.AdvancedRemoteControl;
-import com.patterns2.bridge.SamsungTV;
+import com.patterns2.proxy.Ebook;
+import com.patterns2.proxy.Library;
 import lombok.var;
 
 public class Main {
     public static void main(String[] args) {
-        var remoteControl = new AdvancedRemoteControl(new SamsungTV());
-        remoteControl.turnOn();
+        var library = new Library();
+        String[] fileNames = {"a", "b", "c"};
+        for (var fileName : fileNames) {
+            library.add(new Ebook(fileName));
+        }
+
+        library.openEbook("a");
     }
 }
